c7e763b2a5857a2e06ffe970674ae05e57999d15
Guzman <francisco.guzman@accenture.com>
2024-07-05 14:53:54 -0400

    first commit

---
+++ src/CompareToExample.java
@@ -0,0 +1,10 @@
+public class CompareToExample {
+    public static void main(String[] args) {
+        String s1="hello";
+        String s2="hello";
+        String s3="hemlo";
+        String s4="flag";
+        System.out.println(s1.compareTo(s2)); // 0 because both are equal
+
+    }
+}
